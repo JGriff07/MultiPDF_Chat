15550c2bd6d1892365d48c9f0278d0c774d430ee
JGriff07 <jonathan.griffin.07@gmail.com>
2023-08-15 09:13:49 -0700

    1st Commit - Basic Streamlit Start Page

---
+++ main.py
@@ -0,0 +1,17 @@
+# Import Statements.
+import streamlit as st
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    st.header("Welcome")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
